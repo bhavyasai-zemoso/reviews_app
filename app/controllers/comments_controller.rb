957ac8c25711ec4a4e4f4b395fe33be7133c0c19
bhavyasai-zemoso <bhavya.survepalli@zemosolabs.com>
2021-01-28 17:34:10 +0530

    CRUD api for comments

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,47 @@
+class CommentsController < ApplicationController
+    skip_before_action :verify_authenticity_token
+    before_action :set_comment, only: [:show, :update, :destroy]
+
+  def index
+   @comments = Comment.all
+   render json: @comments
+  end
+
+  def show
+   render json: @comment
+  end
+
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.create(params[:comment].permit(:text))
+    render json: @comment
+  end
+
+  def update
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments
+   if @comment.update(comment_params)
+    render json: @comment
+   else
+    render json: @comment.errors, status: :unprocessable_entity
+   end
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    if @comment.destroy
+        head:ok
+    else
+        render json: @comment.errors
+    end
+  end
+
+  private
+  def set_comment
+   @comment = Comment.find(params[:id])
+  end
+
+  def comment_params
+  params.require(:comment).permit(:text)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,27 @@
 Rails.application.routes.draw do
+
   resources :movies do
     resources :users do
-      resources :posts
+      resources :posts do
+        resources :comments, only: [:index, :show]
+      end
     end
   end
+
   resources :books do
   	resources :users do
-      resources :posts
+      resources :posts do
+        resources :comments, only: [:index, :show]
+      end
     end
   end
+
   resources :users, shallow: true do
   	resources :posts, only: [:index, :show]
   end
   resources :posts
+
+  resources :posts do
+        resources :comments
+      end 
 end
